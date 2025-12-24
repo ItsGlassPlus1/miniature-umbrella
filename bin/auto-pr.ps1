99122d4774eaa7b8b48fbf3ca45a7196a85d13a9
ItsGlassPlus1 <ItsGlassPlus1@proton.me>
2025-12-24 12:16:40 -0800

    Update upstream parameter in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "ItsGlassPlus1/miniature-umbrella:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
